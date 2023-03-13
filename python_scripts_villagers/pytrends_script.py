c5172761d353f192c990199b93ebd5988721890c
Jacqueline Skunda <jskunda@umich.edu>
2023-03-13 16:23:44 -0400

    pytrends_429_errors

--- python_scripts_villagers/pytrends_script.py
+++ python_scripts_villagers/pytrends_script.py
@@ -11,7 +11,13 @@ class fetch_trends:
         self.path_file = path_file
     def get_trends(self):
         # pytrend = TrendReq(hl='en-US', tz=360)
-        pytrend = TrendReq()
+        requests_args = {'headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+        }
+        }
+        
+        # pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), retries=2, backoff_factor=1, requests_args={'verify':False})
+        pytrends = TrendReq(hl='en-US', tz=360, requests_args=requests_args)
         
         acnh_villagers= pd.read_csv(self.path_file + "villagers.csv")
         
@@ -24,16 +30,16 @@ class fetch_trends:
         # fixing OHare again to match
         acnh_villagers_two=["OHare Animal Crossing" if x=="O'Hare Animal Crossing" else x for x in villagers]
 
-        kw_list = acnh_villagers_two
+        kw_listed = acnh_villagers_two
         
         trends = dict()
-        for i in kw_list:
+        for i in kw_listed:
             ##build out query the code commented out was for original run in 2021 during Milestone 1
             # pytrend.build_payload([i],timeframe='2020-03-20 2021-11-24')
-            pytrend.build_payload([i],timeframe='2020-03-20 2023-03-12')
-            time.sleep(60)
+            pytrends.build_payload(kw_list=[i],timeframe='today 5-y')
+            time.sleep(30)
             ##save trend to dictionary
-            trends[i] = pytrend.related_queries()[i]
+            trends[i] = pytrends.related_queries()[i]
 
             # sleep to avoid 429 error 
             time.sleep(60)
