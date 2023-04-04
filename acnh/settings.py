35d9871ac8bbe7ba931f9b2fe42b6e58738e0135
Jacqueline Skunda <jskunda@umich.edu>
2023-04-03 20:22:59 -0400

    settings deploy

--- acnh/settings.py
+++ acnh/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "static"
 
+SESSION_COOKIE_SECURE = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
